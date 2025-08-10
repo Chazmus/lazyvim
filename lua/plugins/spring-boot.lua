1c4540980a31685c361135f55455dccab02fee20
Chaz Bailey <charles.s.bailey@gmail.com>
2025-08-10 17:11:01 +0100

    Add shortcut for debugging spring proj

--- lua/plugins/spring-boot.lua
+++ lua/plugins/spring-boot.lua
@@ -6,10 +6,18 @@ return {
   },
   config = function()
     local springboot_nvim = require("springboot-nvim")
+
+    local function debugWrapper()
+      springboot_nvim.boot_run(
+        "-Dspring-boot.run.jvmArguments='-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005'"
+      )
+    end
+
     vim.keymap.set("n", "<leader>Jr", springboot_nvim.boot_run, { desc = "Spring Boot Run Project" })
+    vim.keymap.set("n", "<leader>Jd", debugWrapper, { desc = "Spring Boot Debug Project" })
     vim.keymap.set("n", "<leader>Jc", springboot_nvim.generate_class, { desc = "Java Create Class" })
     vim.keymap.set("n", "<leader>Ji", springboot_nvim.generate_interface, { desc = "Java Create Interface" })
     vim.keymap.set("n", "<leader>Je", springboot_nvim.generate_enum, { desc = "Java Create Enum" })
-    springboot_nvim.setup({})
+    springboot_nvim.setup()
   end,
 }
